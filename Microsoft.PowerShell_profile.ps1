2d64245a66fdf355b79e145c618029bc35620aa2
Geoffrey Floyd <geoffrey.floyd@hoomanlogic.com>
2015-09-15 21:11:17 -0600

    use environment variable

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,4 @@
 $env:powershell = "$env:HOMEDRIVE$env:HOMEPATH\Documents\WindowsPowerShell"
 . (Resolve-Path "$env:powershell\index.ps1")
+# Load posh-npm example profile
+. (Resolve-Path "$env:powershell\Modules\posh-npm\profile.example.ps1")
